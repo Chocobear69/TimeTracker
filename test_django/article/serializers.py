d206af35ee31aef909c4a73eab59bf894ea52039
vladislav.yun <vladislav.yun@x5.ru>
2020-09-15 18:58:32 +0300

    init

---
+++ test_django/article/serializers.py
@@ -0,0 +1,35 @@
+from rest_framework import serializers
+
+from article.models import Article, Author
+
+
+class ArticleSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Article
+        fields = ("id", "title", "description", "body", "author_id")
+
+
+class AuthorSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Author
+        fields = ("id", "name", "email")
+
+
+class ArticleSerializerFirstLesson(serializers.Serializer):
+    title = serializers.CharField(max_length=120)
+    description = serializers.CharField()
+    body = serializers.CharField()
+    author_id = serializers.IntegerField()
+
+    def create(self, validated_data):
+        return Article.objects.create(**validated_data)
+
+    def update(self, instance, validated_data):
+        instance.title = validated_data.get("title", instance.title)
+        instance.description = validated_data.get("description", instance.description)
+        instance.body = validated_data.get("body", instance.body)
+        instance.author_id = validated_data.get("author_id", instance.author_id)
+
+        instance.save()
+        return instance
+
